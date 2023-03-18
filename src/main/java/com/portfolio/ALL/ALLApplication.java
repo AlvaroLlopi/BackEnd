20f3aa27fb601c519c3f1e15ec7102394126bde1
AlvaroLlopi <llopialvaro@gmail.com>
2023-03-17 21:56:27 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/ALL/ALLApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ALL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ALLApplication.class, args);
+	}
+
+}
